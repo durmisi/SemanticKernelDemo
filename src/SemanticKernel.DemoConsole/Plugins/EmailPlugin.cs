9b146cc628f7d7b479c8f7333ac6bf8deeb6db08
Admir Durmishi <durmisi@gmail.com>
2024-01-17 21:54:10 +0100

    Initial Commit

---
+++ src/SemanticKernel.DemoConsole/Plugins/EmailPlugin.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace SemanticKernelDemo.Plugins
+{
+    /// <summary>
+    /// A plugin that returns the current time.
+    /// </summary>
+    public class EmailPlugin
+    {
+        [KernelFunction]
+        [Description("Sends an email to a recipient.")]
+        public async Task SendEmailAsync(
+            Kernel kernel,
+            [Description("Semicolon delimitated list of emails of the recipients")] string recipientEmails,
+            string subject,
+            string body
+        )
+        {
+            // Add logic to send an email using the recipientEmails, subject, and body
+            // For now, we'll just print out a success message to the console
+            Console.WriteLine("Email sent!");
+        }
+    }
+}
---
+++ src/SemanticKernel.DemoConsole/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Planning.Handlebars;
+using Microsoft.SemanticKernel.Plugins.Core;
+using SemanticKernelDemo.Plugins;
+
+var kernelBuilder = Kernel.CreateBuilder();
+
+//kernelBuilder.Services.AddLogging(c => c.AddDebug().SetMinimumLevel(LogLevel.Trace));
+
+kernelBuilder.AddAzureOpenAIChatCompletion(
+         "gpt-35-turbo-0613",                      // Azure OpenAI Deployment Name
+         "https://{}.openai.azure.com/", // Azure OpenAI Endpoint
+         "");      // Azure OpenAI Key
+
+
+kernelBuilder.Plugins.AddFromType<AuthorEmailPlanner>();
+kernelBuilder.Plugins.AddFromType<EmailPlugin>();
+
+var kernel = kernelBuilder.Build();
+
+
+// Retrieve the chat completion service from the kernel
+IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Create the chat history
+ChatHistory chatMessages = new ChatHistory("""
+You are a friendly assistant who likes to follow the rules. You will complete required steps
+and request approval before taking any consequential actions. If the user doesn't provide
+enough information for you to complete a task, you will keep asking questions until you have
+enough information to complete the task.
+""");
+
+// Start the conversation
+while (true)
+{
+    // Get user input
+    System.Console.Write("User > ");
+    chatMessages.AddUserMessage(Console.ReadLine()!);
+
+    // Get the chat completions
+    OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+    {
+        ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
+    };
+
+    var result = chatCompletionService.GetStreamingChatMessageContentsAsync(
+        chatMessages,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Stream the results
+    string fullMessage = "";
+    await foreach (var content in result)
+    {
+
+        if(content.Content == null)
+        {
+            continue;
+        }
+
+        if (content.Role.HasValue)
+        {
+            System.Console.Write("Assistant > ");
+        }
+        System.Console.Write(content.Content);
+        fullMessage += content.Content;
+    }
+    System.Console.WriteLine();
+
+    // Add the message from the agent to the chat history
+    chatMessages.AddAssistantMessage(fullMessage);
+}
